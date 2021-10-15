79797c069a75845ba19e42707e485587bf394281
SergeyLizunov <s.lizunov2020@yandex.ru>
2021-10-15 23:07:01 +0300

    StartJava_lesson_5

---
+++ src/com/startjava/lesson5/create_db.sql
@@ -0,0 +1,9 @@
+create table jaegers (
+id serial primary key,
+modelName character varying(30),
+mark character varying(10),
+height numeric(4, 2),
+weight numeric(4, 3),
+status character varying(10),
+origin character varying(10),
+launch integer, kaijuKill integer);
---
+++ src/com/startjava/lesson5/init_db.sql
@@ -0,0 +1,12 @@
+insert into jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill) 
+values
+(‘Acid Geisha’, ‘Mark-6’, 83.26, 2.532, ‘Destroyed’, ‘Japan’, 2021, 1),
+(‘Apex’, ‘Mark-4’, 74.31, 2.358, ‘Active’, ‘USA’, 2035, 3),
+(‘Atlas Destroyer’, ‘Mark-3’, 68.27, 2.214, ‘Active’, ‘Russia’, 2017, 1),
+(‘Black Maria’, ‘Mark-3’, 72.11, 2.087, ‘Active’, ‘Mexico’, 2019, 2),
+(‘Bracer Phoenix’, ‘Mark-5’, 70.91, 2.128, ‘Destroyed’, ‘Germany’, 2025, 4),
+(‘Brawler Yukon’, ‘Mark-1’, 72.74, 1.928, ‘Destroyed’, ‘USA’, 2015, 2),
+(‘Chaos Nemesis’, ‘Mark-4’, 85.28, 2.736, ‘Destroyed’, ‘USSR’, 1981, 5),
+(‘Chrome Brutus’, ‘Mark-3’, 77.24, 2.414, ‘Destroyed’, ‘Canada’, 2017, 0),
+(‘Chronos Berserker’, ‘Mark-5’, 72.21, 2.101, ‘Active’, ‘Australia’, 2019, 1),
+(‘Diablo Intercept’, ‘Mark-2’, 70.01, 1.999, ‘Destroyed’, ‘Chile’, 2016, 2);
